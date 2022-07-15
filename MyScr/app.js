ea6b12cab359d772107ea21ba906eaaabc5a3f7f
Rosalba Vázquez <rosalbamusician@gmail.com>
2022-07-14 20:06:15 -0600

    Pagina 1 con insertar nombre y opciones voladores, finito. Ya se va a mensaje. Ahora toca trabajar en mensaje y tal vez cambiar el background de la pagina 1

--- MyScr/app.js
+++ MyScr/app.js
@@ -2,4 +2,7 @@
 function saludo (){
     var Name = document.getElementById("name").value;
     document.getElementById("dialogtext").innerHTML = "Hola " + Name + ". ¿Qué te gustaría hacer hoy?"
+    const Opciones = document.getElementById("opciones")
+    Opciones.style.display = "block"
 }
+
