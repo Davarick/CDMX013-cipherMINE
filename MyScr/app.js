37112abc7da3996e0622908d780577de3d50820f
Rosalba Vázquez <rosalbamusician@gmail.com>
2022-07-17 20:55:56 -0600

    Ya se como hacer el cifrado, y, como pensaba, no esta tan difici

--- MyScr/app.js
+++ MyScr/app.js
@@ -6,3 +6,16 @@ function saludo (){
     Opciones.style.display = "block"
 }
 
+function getthememo (){
+var theTextMessage = document.getElementById("themessage").value;
+window.location.href = "offset.html";
+}
+
+function codethememo () 
+{
+    var theOffset = document.getElementById("offset").value;
+    var codedTextMesagge;
+    for (var i = 0; i < theTextMessage.length; i++) {
+        codedTextMesagge[i] = (theTextMessage[i] - 65 + theOffset) % 26 + 65;  
+      }
+}
