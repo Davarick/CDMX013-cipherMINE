6c8553afd9e90e6de8039f15a93178e72a6a8faf
Rosalba Vázquez <rosalbamusician@gmail.com>
2022-07-18 12:12:29 -0600

    Cambiando a node. Hay que investigar

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 
+ let theOffset;
 function saludo (){
     var Name = document.getElementById("name").value;
     document.getElementById("dialogtext").innerHTML = "Hola " + Name + ". ¿Qué te gustaría hacer hoy?"
@@ -11,9 +12,20 @@ var theTextMessage = document.getElementById("themessage").value;
 window.location.href = "offset.html";
 }
 
+function insertoffset () {
+document.getElementById("img-circuloexterno").animate([
+    // fotogramas clave
+    { transform: "rotate(0turn)" },
+    { transform: 'rotate((1turn))' }
+  ], {
+    // opciones de sincronizaciónd
+    duration:   1000,
+    iterations: 1
+  });
+}
+
 function codethememo () 
-{
-    var theOffset = document.getElementById("offset").value;
+{  
     var codedTextMesagge;
     for (var i = 0; i < theTextMessage.length; i++) {
         codedTextMesagge[i] = (theTextMessage[i] - 65 + theOffset) % 26 + 65;  
