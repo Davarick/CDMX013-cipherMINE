4caa0a4dc2e138a409b9ed31e3a36fa6347c7ca4
Rosalba Vázquez <rosalbamusician@gmail.com>
2022-07-18 16:53:11 -0600

    voy a cambiar la estructura, por eso guardo antes

--- src/app.js
+++ src/app.js
@@ -1,30 +1,39 @@
 
- let theOffset;
+let theOffset;
+let theTextMessage = "Fuck";
 function saludo (){
-    var Name = document.getElementById("name").value;
+    let Name = document.getElementById("name").value;
     document.getElementById("dialogtext").innerHTML = "Hola " + Name + ". ¿Qué te gustaría hacer hoy?"
     const Opciones = document.getElementById("opciones")
     Opciones.style.display = "block"
 }
 
 function getthememo (){
-var theTextMessage = document.getElementById("themessage").value;
+theTextMessage = document.getElementById("themessage").value;
+console.log(theTextMessage);
 window.location.href = "offset.html";
 }
 
+let circuloExterno;
 const root = document.documentElement;
 function insertoffset () {
-  theOffset = document.getElementById("offset").value
-  root.style.setProperty('--turn', (theOffset * (1/26)) + "turn"); 
-  document.getElementById('img-circuloexterno').style.animation="girarcipher 2s 1 forwards";
+  circuloExterno = document.getElementById('img-circuloexterno');
+theOffset = document.getElementById("offset").value
+root.style.setProperty('--turn', (theOffset * (1/26)) + "turn");
+//No entiendo
+circuloExterno.style.animation = 'none';
+circuloExterno.offsetHeight; /* trigger reflow */
+circuloExterno.style.animation = ""; 
+circuloExterno.style.animation="girarcipher 2s 1 forwards";
+console.log(theTextMessage);
 }
-
-
 
 function codethememo () 
 {  
-    var codedTextMesagge;
+    let codedTextMesagge;
     for (var i = 0; i < theTextMessage.length; i++) {
         codedTextMesagge[i] = (theTextMessage[i] - 65 + theOffset) % 26 + 65;  
       }
+      window.location.href = "result.html";
+      console.log(codedTextMesagge);
 }
